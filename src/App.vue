cc5d13a2fc4da8d16c4e60ad63a96ff21dc4461a
Luca Pulira <luca.pulira@gmail.com>
2023-10-06 19:08:34 +0200

    new version

--- src/App.vue
+++ src/App.vue
@@ -3,63 +3,63 @@
     <form class="form-test" @submit="submit">
       <h1>Code input example</h1>
       <InputPin
-          class="flex-1"
-          numeberOfDigits="6"
-          v-model="otp"
-          border-color="#9CA3AF"
-          focused-border-color="#FCD34D"
-          border-width="4px"
-          radius="12px"
-          bg-color="#efefef"
+        class="flex-1"
+        numeberOfDigits="6"
+        v-model="otp"
+        border-color="#9CA3AF"
+        focused-border-color="#FCD34D"
+        border-width="2px"
+        radius="250px"
+        bg-color="#efefef"
       ></InputPin>
-      <div class="code-box">{{otp}}</div>
+      <div class="code-box">{{ otp }}</div>
     </form>
   </div>
 </template>
 <script setup lang="ts">
-import 'lp741-vue-components/style.css'
-import {ref} from 'vue'
-import { InputPin } from 'lp741-vue-components'
-const submit = () => {
-  console.log('submitted')
-}
+  import 'lp741-vue-components/style.css'
+  import { ref } from 'vue'
+  import { InputPin } from 'lp741-vue-components'
 
-const otp = ref('');
+  const submit = () => {
+    console.log('submitted')
+  }
 
+  const otp = ref('')
 </script>
 <style scoped>
-.container--centered {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-height: 100vh;
-  width: 100%;
-}
+  .container--centered {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    width: 100%;
+  }
 
-.form-test {
-  padding: 1rem;
-  width: 100%;
-  max-width: 640px;
-  margin-left: auto;
-  margin-right: auto;
-  background-color: rgba(240,240,240,0.4);
-  min-height: 320px;
-  display: flex;
-  gap: 2rem;
-  flex-direction: column;
-  align-items: center;
-}
+  .form-test {
+    padding: 1rem;
+    width: 100%;
+    max-width: 640px;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: rgba(240, 240, 240, 0.4);
+    min-height: 320px;
+    display: flex;
+    gap: 2rem;
+    flex-direction: column;
+    align-items: center;
+  }
 
-.code-box {
-  background-color: #fff;
-  font-size: 3rem;
-  padding: 1rem 2rem;
-  font-family: "Courier New", sans-serif;
-}
+  .code-box {
+    background-color: #fff;
+    font-size: 3rem;
+    padding: 1rem 2rem;
+    font-family: 'Courier New', sans-serif;
+  }
 
-.flex-1 {
-  flex: 1;
-}
-
+  .flex-1 {
+    width: 100%;
+    justify-content: space-around;
+    flex: 1;
+  }
 </style>
-
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -7,4 +8,10 @@ export default defineConfig({
   server: {
     port: 3000
   },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      './runtimeConfig': './runtimeConfig.browser',
+    }
+  }
 })
